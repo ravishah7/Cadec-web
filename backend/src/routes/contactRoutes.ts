7e9f19475bd4ae30eed25d5f1d7136234c0c469b
Ravi Shah <ravijasodashah@gmail.com>
2026-07-15 02:43:34 +0530

    update

--- backend/src/routes/contactRoutes.ts
+++ backend/src/routes/contactRoutes.ts
@@ -1,6 +1,6 @@
 // backend/src/routes/contactRoutes.ts
 import express from 'express';
-import { submitContact, getContacts, getContact, markResolved, deleteContact } from '../controllers/contactController';
+import { submitContact,} from '../controllers/contactController';
 import { authenticate, requireAdmin } from '../middleware/auth';
 
 const router = express.Router();
